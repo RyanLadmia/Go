5164294bd79e0e84a7fdb110e87816a498e3f497
ryan-ladmia <ryan.ladmia@laplateforme.io>
2025-09-08 18:11:44 +0200

    Début de la leçon sur les modules.

--- lesson18/main.go
+++ lesson18/main.go
@@ -3,5 +3,17 @@ package main
 /*
 
 Les modules :
+En Go, un module est un ensemble de packages versionné, qui sert à organiser et gérer le code ainsi que ses dépendances.
+
+1.	Module = projet ou bibliothèque Go
+	•	Contient un fichier go.mod à sa racine.
+	•	Un module peut contenir plusieurs packages.
+2.	Fichier go.mod
+	•	Définit le nom du module et les versions des dépendances utilisées.
+3.	But principal
+	•	Gérer les packages et leurs versions.
+	•	Faciliter l’importation du code entre différents projets.
+
+Un module Go est un projet avec un go.mod qui contient un ou plusieurs packages et qui gère les dépendances de manière versionnée.
 
 */
